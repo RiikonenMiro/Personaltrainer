f5e81e104235ff26c1c143f56ebd97249b916125
Miro Riikonen <bgp009@myy.haaga-helia.fi>
2021-12-02 13:18:32 +0200

    Task 1 done

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
+import { format, parseISO } from 'date-fns';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
@@ -24,7 +25,14 @@ export default function Traininglist() {
 
     const columns = [
         { field: 'activity', sortable: true, filter: true },
-        { field: 'date', sortable: true, filter: true },
+        { field: 'date', sortable: true, filter: true, cellRendererFramework: function (params){
+            return (
+                <p>{format(parseISO(params.data.date), 'dd.MM.yyyy hh:mm a', { timeZone: "UTC" })}</p>
+            );
+            }, getQuickFilterText: function (params) {
+                return format(parseISO(params.data.date), 'dd.MM.yyyy hh:mm a', { timeZone: "UTC" });
+            }
+        },
         { field: 'duration', sortable: true, filter: true },
     ]
 
